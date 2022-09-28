a2078ab0454c04aa0d4026d714a07cc4853c9aaa
Sriram Raju <sriram.raju01@gmail.com>
2022-09-27 23:30:06 -0500

    Polymorphism

---
+++ module-1/12_Polymorphism/exercise/src/main/java/com/techelevator/BankCustomer.java
@@ -0,0 +1,62 @@
+package com.techelevator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankCustomer {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private List<Accountable> accounts = new ArrayList<Accountable>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Accountable[] getAccounts() {
+        Accountable[] accountArray = new Accountable[accounts.size()];
+        for (int i =0; i < accountArray.length; i++) {
+            accountArray[i] = accounts.get(i);
+        }
+        return accountArray;
+    }
+
+    public void addAccount(Accountable newAccount) {
+        accounts.add(newAccount);
+    }
+
+    public boolean isVip() {
+        System.out.println(accounts.toString());
+        System.out.println(accounts.size());
+        int balance = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            balance += accounts.get(i).getBalance();
+        }
+        if (balance >= 25000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
